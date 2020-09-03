6a1a97ac3cd13fce85bf280fd316e59ca3c240f7
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-09-03 20:57:30 -0300

    Establishing application routing with react-router-dom.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './components/Home';
+import Genre from './components/Genre';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Home />
-    </>
+      <div className="App">
+        <Switch>
+          <Route path='/' exact component={ Home } />
+          <Route path='/generos' component={ Genre } />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
