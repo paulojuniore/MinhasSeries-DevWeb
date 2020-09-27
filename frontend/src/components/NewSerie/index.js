78ecd50d0cb85288ac8fca0e7da75bd553637004
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-09-27 08:34:03 -0300

    Capturando campos do form de nova serie.

--- frontend/src/components/NewSerie/index.js
+++ frontend/src/components/NewSerie/index.js
@@ -2,19 +2,39 @@ import React, { useState, useEffect } from 'react';
 import { 
   Form, 
   FormGroup, 
-  Button,
-  Checkbox, 
+  Button
 } from 'react-bootstrap';
 
 import api from '../../services/api';
 
 const NewSerie = () => {
-  const [generos, setGeneros] = useState([])
+  const [generos, setGeneros] = useState([]);
+  const [serie, setSerie] = useState('');
+  const [genero, setGenero] = useState('');
+  const [classificacao, setClassificacao] = useState('');
+  const [ehFilme, setEhFilme] = useState(false);
+  const [ehFavorito, setEhFavorito] = useState(false);
 
   useEffect(() => {
     api.get('/generos')
       .then(response => setGeneros(response.data))
-  })
+  });
+
+  function handleChangeGenreType(e) {
+    setGenero(e.target.value);
+  }
+
+  function handleChangeClassificationType(e) {
+    setClassificacao(e.target.value);
+  }
+
+  function handleChangeIsMovie(e) {
+    setEhFilme(e.target.checked);
+  }
+
+  function handleChangeIsFavorite(e) {
+    setEhFavorito(e.target.checked);
+  }
 
   return (
     <div className="container">
@@ -22,15 +42,27 @@ const NewSerie = () => {
       <Form>
         <FormGroup controlId="formBasic">
           <Form.Label style={{ marginTop: 15 }}>Nome do(a) série/filme:</Form.Label>
-          <Form.Control type="text" placeholder="Nome do(a) série/filme" />
+          <Form.Control 
+            type="text" 
+            placeholder="Nome do(a) série/filme"
+            value={ serie }
+            onChange={ (e) => setSerie(e.target.value) } />
 
           <Form.Label style={{marginTop: 35}}>Selecione o gênero:</Form.Label>
-          <Form.Control as="select" custom style={{width: 600, marginLeft: 30}}>
+          <Form.Control 
+            as="select" 
+            custom 
+            style={{width: 600, marginLeft: 30}} 
+            onChange={ handleChangeGenreType }
+          >
             { generos.map(genero => <option>{ genero.genero }</option>) }
           </Form.Control>
 
           <Form.Label style={{marginTop: 35}}>Selecione a classificação indicativa:</Form.Label>
-          <Form.Control as="select" custom style={{width: 600, marginLeft: 30}}>
+          <Form.Control 
+            as="select" custom 
+            style={{width: 600, marginLeft: 30}} 
+            onChange={ handleChangeClassificationType }>
             <option>L (Livre)</option>
             <option>+10</option>
             <option>+12</option>
@@ -47,7 +79,8 @@ const NewSerie = () => {
               type="checkbox" 
               class="form-check-input" 
               id="exampleCheck1"
-              style={{ marginLeft: 15, width: 20, height: 20 }} />
+              style={{ marginLeft: 15, width: 20, height: 20 }}
+              onChange={ handleChangeIsMovie } />
           </div>
 
           <div className="form-check" style={{ marginTop: 20 }}>
@@ -56,7 +89,8 @@ const NewSerie = () => {
               type="checkbox" 
               class="form-check-input" 
               id="exampleCheck1"
-              style={{ marginLeft: 15, width: 20, height: 20 }} />
+              style={{ marginLeft: 15, width: 20, height: 20 }}
+              onChange={ handleChangeIsFavorite } />
           </div>
         </FormGroup>
       </Form>
