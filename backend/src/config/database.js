08665abd14d0f80514dec8452b5f36301aee086b
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2020-10-11 14:12:36 -0300

    Added method put to update series.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,20 +2,20 @@ require('dotenv/config');
 
 module.exports = {
   dialect: process.env.DB_DIALECT,
-  host: process.env.DB_PROD_HOST,
-  username: process.env.DB_PROD_USER,
-  password: process.env.DB_PROD_PASSWORD,
-  database: process.env.DB_PROD_DBNAME,
+  host: process.env.DB_DEV_HOST,
+  username: process.env.DB_DEV_USER,
+  password: process.env.DB_DEV_PASSWORD,
+  database: process.env.DB_DEV_DBNAME,
   define: {
     timeStamps: false,
     underscored: true,
   },
-  dialectOptions: { 
-    native: true,
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, 
-    }
-  },
-  ssl: true,
+  // dialectOptions: { 
+  //   native: true,
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false, 
+  //   }
+  // },
+  // ssl: true,
 }
